81ba8a9929dafeae0854202fd46918bf5b6366ea
Dibyendu Sikdar <dibyendu.coder@gmail.com>
2020-04-28 04:05:38 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -4,6 +4,8 @@ import sys
 import shutil
 import subprocess
 import re
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #These endpoints will verify if the .git dir is available and then it will create them if required
 
 def rebuild_folders(folders,base):
@@ -17,7 +19,7 @@ def rebuild_folders(folders,base):
 def rebuild_objects(endpoints,base):
 	for endpoint in endpoints:
 		try:
-			req = requests.get(url+base+endpoint,stream=True)
+			req = requests.get(url+base+endpoint,stream=True,verify=False)
 			if req.status_code == 200:
 				#print("Found "+endpoint+" =>"+req.text)
 				##if the endpoint is found, the necessary files are created
@@ -37,7 +39,9 @@ if len(sys.argv) < 2:
 url = sys.argv[1]+'/.git/'
 #Base files and folders
 
-
+#os.system("git init")
+os.system("mkdir .git")
+os.chdir(".git")
 
 folders1 = ['branches','hooks','info','logs','objects','refs']
 rebuild_folders(folders1,'')
@@ -58,54 +62,91 @@ rebuild_folders(refs_folders,'refs/')
 rebuild_objects(refs_files,'refs/')
 
 #Start creating the objects
-#Get the base object id from : logs/HEAD
+#First fix all from logs/HEAD
+
+'''
 w = open('logs/HEAD','r')
 object_id=w.readline().split(" ")[1]
 w.close()
 print(object_id)
+'''
+
+object_ids = []
+
+with open('logs/HEAD') as f:
+	for line in f:
+		object_id1=line.split(" ")[0]
+		object_id2=line.split(" ")[1]
+		object_ids.append(object_id1)
+		object_ids.append(object_id2)
 
 
+for object_id in object_ids:
+	object_location = object_id[0:2]+"/"+object_id[2:]
+	print(object_location)
+	object_folders=[object_id[0:2]]
+	object_files = [object_location]
+	rebuild_folders(object_folders,'objects/')
+	rebuild_objects(object_files,'objects/')
+
+'''
 object_location = object_id[0:2]+"/"+object_id[2:]
 print(object_location)
 object_folders=[object_id[0:2]]
 object_files = [object_location]
 rebuild_folders(object_folders,'objects/')
 rebuild_objects(object_files,'objects/')
-
+'''
 #Now fix the remaining objects by checking the errors
-while True:
-	print(os.getcwd())
-	os.chdir("../")
 
-	out = subprocess.Popen(['git', 'checkout','-f'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-	stdout,stderr = out.communicate()
-	print(stdout)
-
-
-
-	error = stdout.decode('utf-8').strip()
-	pos = error.find("error:")
-	if pos == -1:
-		break
+cmds = [
 	
-	
-	shas = re.findall(r"\b[0-9a-f]{40}\b",error)
-	print(shas)
-	os.chdir(".git")
-	
-	for object_id in shas:
-		#print(object_id)
-		#print(os.getcwd())
+	['git', 'checkout','-f'],
+	['git','log']
+]
+
+for cmd in cmds:
+
+	while True:
+		print(os.getcwd())
+		os.chdir("../")
+		out = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+		#out = subprocess.Popen(['git', 'checkout','-f'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+		stdout,stderr = out.communicate()
+		print(stdout)
+
+
+
+		error = stdout.decode('utf-8').strip()
+		pos1 = error.find("error:")
+		pos2 = error.find("fatal:")
+		if pos1 == -1 and pos2 == -1:
+			break
+		if error.find("You are on a branch yet to be born") >= 0:
+			break
+		if error.find("not a git repository (or any of the parent directories)") >=0:
+			break;
 		
-		object_location = object_id[0:2]+"/"+object_id[2:]
-		print(object_location)
-		object_folders=[object_id[0:2]]
-		object_files = [object_location]
-		rebuild_folders(object_folders,'objects/')
-		rebuild_objects(object_files,'objects/')
-		#print(os.getcwd())
-	    #os.chdir(".git")
+		
+		shas = re.findall(r"\b[0-9a-f]{40}\b",error)
+		print(shas)
+		os.chdir(".git")
+		
+		for object_id in shas:
+			#print(object_id)
+			#print(os.getcwd())
+			
+			object_location = object_id[0:2]+"/"+object_id[2:]
+			print(object_location)
+			object_folders=[object_id[0:2]]
+			object_files = [object_location]
+			rebuild_folders(object_folders,'objects/')
+			rebuild_objects(object_files,'objects/')
+			#print(os.getcwd())
+			#os.chdir(".git")
 
 
 
 
+
+print("Now issue `git checkout -f`")
